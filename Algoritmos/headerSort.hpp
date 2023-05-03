55dc860c610f35b3bc04cbfdeb725d18b258bc28
Brenda Romero <brenda.romero@ucr.ac.cr>
2023-05-02 23:48:21 -0600

    Documentando código del archivo headerSort.hpp
    Signed-off-by: Brenda Romero <brenda.romero@ucr.ac.cr>

--- Algoritmos/headerSort.hpp
+++ Algoritmos/headerSort.hpp
@@ -1,37 +1,68 @@
 /**
-Licencia MIT
+ * @file headerSort.hpp
+ * @author Brenda Romero Solano (brenda.romero@ucr.ac.cr)
+ * @brief Declaración de las funciones para los distintos tipos de algoritmos
+ * de ordenamiento.
+ * @version 0.1
+ * @date 2023-05-02
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ * Licencia MIT
 
-Copyright (c) [2023] Tarea 2 - Grupo 2 - IE 0217 - I Ciclo 2023
-
-Se concede aquí, sin cargo, a cualquier persona que obtenga una copia de este
-software y los archivos de documentación asociados (el "Software"), para
-utilizar el Software sin restricciones, incluyendo, sin limitación, los
-derechos para usar, copiar, modificar, fusionar, publicar, distribuir,
-sublicenciar y/o vender copias del Software, y para permitir a las personas a
-quienes se les proporcione el Software que lo hagan, sujeto a las siguientes
-condiciones:
-
-El aviso de copyright anterior y este aviso de permiso se incluirán en todas
-las copias o partes sustanciales del Software.
-
-EL SOFTWARE SE PROPORCIONA "TAL CUAL", SIN GARANTÍA DE NINGÚN TIPO, EXPRESA O
-IMPLÍCITA, INCLUYENDO PERO NO LIMITADO A GARANTÍAS DE COMERCIALIZACIÓN,
-IDONEIDAD PARA UN PROPÓSITO PARTICULAR Y NO INFRACCIÓN. EN NINGÚN CASO LOS
-AUTORES O TITULARES DEL COPYRIGHT SERÁN RESPONSABLES POR NINGUNA RECLAMACIÓN,
-DAÑO O OTRA RESPONSABILIDAD, YA SEA EN UNA ACCIÓN CONTRACTUAL, AGRAVIO O DE
-OTRO MODO, QUE SURJA DE, FUERA DE O EN CONEXIÓN CON EL SOFTWARE O EL USO U
-OTROS ACUERDOS EN EL SOFTWARE.
-*/
+ * Copyright (c) [2023] Tarea 2 - Grupo 2 - IE 0217 - I Ciclo 2023
+ * Se concede aquí, sin cargo, a cualquier persona que obtenga una copia de este
+ * software y los archivos de documentación asociados (el "Software"), para
+ * utilizar el Software sin restricciones, incluyendo, sin limitación, los
+ * derechos para usar, copiar, modificar, fusionar, publicar, distribuir,
+ * sublicenciar y/o vender copias del Software, y para permitir a las personas a
+ * quienes se les proporcione el Software que lo hagan, sujeto a las siguientes
+ * condiciones:
+ * 
+ * El aviso de copyright anterior y este aviso de permiso se incluirán en todas
+ * las copias o partes sustanciales del Software.
+ * 
+ * EL SOFTWARE SE PROPORCIONA "TAL CUAL", SIN GARANTÍA DE NINGÚN TIPO, EXPRESA O
+ * IMPLÍCITA, INCLUYENDO PERO NO LIMITADO A GARANTÍAS DE COMERCIALIZACIÓN,
+ * IDONEIDAD PARA UN PROPÓSITO PARTICULAR Y NO INFRACCIÓN. EN NINGÚN CASO LOS
+ * AUTORES O TITULARES DEL COPYRIGHT SERÁN RESPONSABLES POR NINGUNA RECLAMACIÓN,
+ * DAÑO O OTRA RESPONSABILIDAD, YA SEA EN UNA ACCIÓN CONTRACTUAL, AGRAVIO O DE
+ * OTRO MODO, QUE SURJA DE, FUERA DE O EN CONEXIÓN CON EL SOFTWARE O EL USO U
+ * OTROS ACUERDOS EN EL SOFTWARE.
+ */
 
 #ifndef HEADER_SORT_HPP
-#def HEADER_SORT_HPP
-
+#define HEADER_SORT_HPP
+/**
+ * @brief Declaración del método que utiliza el algoritmo BubbleSort.
+ * 
+ * @param arr Números a ordenar.
+ * @param n 
+ */
 void bubbleSort(int arr[], int n);
 
+/**
+ * @brief Declaración del método que utiliza el algoritmo selectionSort.
+ * 
+ * @param arr Números a ordenar.
+ * @param n 
+ */
 void selectionSort(int arr[], int n);
 
+/**
+ * @brief Declaración del método que utiliza el algoritmo insertionSort.
+ * 
+ * @param arr Números a ordenar.
+ * @param n 
+ */
 void insertionSort(int arr[], int n);
 
+/**
+ * @brief Declaración del método que utiliza el algoritmo quickSort.
+ * 
+ * @param arr Números a ordenar.
+ * @param n 
+ */
 void quickSort(int arr[], int low, int high);
 
 #endif /* HEADER_SORT_HPP */
