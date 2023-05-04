36f7295d9b75174fbd9774001561539690c19952
Brenda Romero <brenda.romero@ucr.ac.cr>
2023-05-04 06:15:53 -0600

    Implementación y documentación para el proyecto de algoritmos de ordenamiento
    Signed-off-by: Brenda Romero <brenda.romero@ucr.ac.cr>

--- Algoritmos/headerSort.hpp
+++ Algoritmos/headerSort.hpp
@@ -1,14 +1,14 @@
 /**
  * @file headerSort.hpp
  * @author Brenda Romero Solano (brenda.romero@ucr.ac.cr)
- * @brief Declaración de las funciones para los distintos tipos de algoritmos
+ * @brief Declaración de las funciones para los diferentes métodos
  * de ordenamiento.
  * @version 0.1
  * @date 2023-05-02
  * 
  * @copyright Copyright (c) 2023
  * 
- * Licencia MIT
+ * @see https://opensource.org/licenses/MIT Licencia MIT
 
  * Copyright (c) [2023] Tarea 2 - Grupo 2 - IE 0217 - I Ciclo 2023
  * Se concede aquí, sin cargo, a cualquier persona que obtenga una copia de este
@@ -33,36 +33,43 @@
 
 #ifndef HEADER_SORT_HPP
 #define HEADER_SORT_HPP
+
+#include <iostream>
+
+using namespace std;
+
 /**
  * @brief Declaración del método que utiliza el algoritmo BubbleSort.
  * 
- * @param arr Números a ordenar.
- * @param n 
+ * @param arr Arreglo de números a ordenar.
+ * @param n Tamaño del array de números recibidos.
  */
 void bubbleSort(int arr[], int n);
 
 /**
  * @brief Declaración del método que utiliza el algoritmo selectionSort.
  * 
- * @param arr Números a ordenar.
- * @param n 
+ * @param arr Arreglo de números a ordenar.
+ * @param n Tamaño del array de números recibidos.
  */
 void selectionSort(int arr[], int n);
 
 /**
  * @brief Declaración del método que utiliza el algoritmo insertionSort.
  * 
- * @param arr Números a ordenar.
- * @param n 
+ * @param arr Arreglo de números a ordenar.
+ * @param n Tamaño del array de números recibidos.
  */
 void insertionSort(int arr[], int n);
 
 /**
  * @brief Declaración del método que utiliza el algoritmo quickSort.
  * 
- * @param arr Números a ordenar.
- * @param n 
+ * @param arr Arreglo de números a ordenar.
+ * @param low Índice menor del subarreglo a ordenar
+ * @param high Índice mayor del subarreglo a ordenar
+ * @param n Tamaño del array de números recibidos.
  */
-void quickSort(int arr[], int low, int high);
+void quickSort(int arr[], int low, int high,int n);
 
 #endif /* HEADER_SORT_HPP */
